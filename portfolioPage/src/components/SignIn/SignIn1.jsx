d7976c8c80d0a810f87b8fb2a925da2e52bbfa57
Labib Khan <labibkhan11@8a-29-f0-82-11-13.wireless.temple.edu>
2025-04-25 12:27:47 -0400

    Fixed app.jsx

--- portfolioPage/src/components/SignIn/SignIn1.jsx
+++ portfolioPage/src/components/SignIn/SignIn1.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import bg from "../../assets/images/bg.jpg";
-import "./SigninPage.css";
+import "./Signinpage.css";
 
 const GSignIn1 = () => {
   const navigate = useNavigate();
